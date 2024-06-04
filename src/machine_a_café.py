922d0bce13e99a6c25b7d3cf5f427f990fae84f4
Enzo Sandre <enzo@enzosandre.fr>
2024-06-04 10:02:02 +0200

    Cas nominal

---
+++ src/machine_a_café.py
@@ -0,0 +1,7 @@
+class MachineACafé:
+    def __init__(self):
+        self.somme_encaissée_en_centimes = 100
+        self.nombre_cafés_servis = 1
+
+    def insérer(self, montant):
+        pass
--- test/test_service.py
+++ test/test_service.py
@@ -1,9 +1,22 @@
 import unittest
 
+from machine_a_café import MachineACafé
+from pièce import Pièce
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(False, False)  # add assertion here
+    def test_cas_nominal(self):
+        # ETANT DONNE une machine à café
+        machine = MachineACafé()
+
+        # QUAND on insère 1€
+        machine.insérer(Pièce.UnEuro)
+
+        # ALORS l'ordre de couler un café est envoyé
+        self.assertEqual(1, machine.nombre_cafés_servis)
+
+        # ET le monnayeur contient l'argent
+        self.assertEqual(100, machine.somme_encaissée_en_centimes)
 
 
 if __name__ == '__main__':
