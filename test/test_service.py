6b02a7f1d09daf2c40bb8994961cb8edbfc377c5
Enzo Sandre <enzo@enzosandre.fr>
2024-06-04 10:13:30 +0200

    Factorisation des tests

--- test/test_service.py
+++ test/test_service.py
@@ -1,40 +1,30 @@
+import random
 import unittest
 
 from machine_a_café import MachineACafé
 from pièce import Pièce
 
 
-class MyTestCase(unittest.TestCase):
-    def test_cas_nominal(self):
-        # ETANT DONNE une machine à café
-        machine = MachineACafé()
-        somme_initiale = machine.somme_encaissée_en_centimes
-        nombre_cafés_initiaux = machine.nombre_cafés_servis
+class ServiceTest(unittest.TestCase):
+    def test_n_cafés(self):
+        cas = [1, 2, random.Random().randint(3, 100)]
 
-        # QUAND on insère 1€
-        machine.insérer(Pièce.UnEuro)
+        for nombre_cafés_servis in cas:
+            with self.subTest(nombre_cafés_servis):
+                # ETANT DONNE une machine à café
+                machine = MachineACafé()
+                somme_initiale = machine.somme_encaissée_en_centimes
+                nombre_cafés_initiaux = machine.nombre_cafés_servis
 
-        # ALORS l'ordre de couler un café est envoyé
-        self.assertEqual(1, machine.nombre_cafés_servis - nombre_cafés_initiaux)
+                # QUAND on insère 1€, <n> fois
+                for n in range(nombre_cafés_servis):
+                    machine.insérer(Pièce.UnEuro)
 
-        # ET le monnayeur contient l'argent
-        self.assertEqual(100, machine.somme_encaissée_en_centimes - somme_initiale)
+                # ALORS l'ordre de couler un café est envoyé 2 fois
+                self.assertEqual(nombre_cafés_servis , machine.nombre_cafés_servis - nombre_cafés_initiaux)
 
-    def test_2_cafés(self):
-        # ETANT DONNE une machine à café
-        machine = MachineACafé()
-        somme_initiale = machine.somme_encaissée_en_centimes
-        nombre_cafés_initiaux = machine.nombre_cafés_servis
-
-        # QUAND on insère 1€, deux fois
-        machine.insérer(Pièce.UnEuro)
-        machine.insérer(Pièce.UnEuro)
-
-        # ALORS l'ordre de couler un café est envoyé 2 fois
-        self.assertEqual(2, machine.nombre_cafés_servis - nombre_cafés_initiaux)
-
-        # ET le monnayeur contient l'argent en double
-        self.assertEqual(200, machine.somme_encaissée_en_centimes - somme_initiale)
+                # ET le monnayeur contient l'argent en double
+                self.assertEqual(100 * nombre_cafés_servis, machine.somme_encaissée_en_centimes - somme_initiale)
 
 
 if __name__ == '__main__':
